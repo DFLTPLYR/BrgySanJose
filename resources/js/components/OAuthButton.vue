c98a967cf33338d9ef793f24d8c4027caf9bda68
kobe san juan <sanjuanvincekobef@gmail.com>
2025-09-19 18:36:26 +0800

    login

---
+++ resources/js/components/OAuthButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+    class="flex h-12 w-full items-center justify-center rounded-md border bg-white shadow hover:bg-gray-50"
+  >
+    <Icon :icon="icon" class="h-6 w-6" />
+  </button>
+</template>
+
+<script setup>
+import { Icon } from "@iconify/vue";
+
+defineProps({
+  provider: String,
+  icon: String,
+});
+</script>
+
