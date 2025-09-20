b4c5301c3eff8b131d7e470c6106152799fbe7a8
DFLTPLYR <gonzales.johncris01@gmail.com>
2025-09-20 17:26:08 +0800

    Refactor project structure: update package.json and package-lock.json, add Sidebar component, and create Clearance and Accounts pages with routing

---
+++ resources/js/pages/Accounts.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Layout from '@/layouts/Layout.vue';
+</script>
+
+<template>
+    <Layout>
+        <div class="bg-white p-8 rounded-xl shadow w-full space-y-6">
+            <h1 class="text-3xl font-extrabold text-gray-800 mb-2">Accounts Dashboard</h1>
+        </div>
+    </Layout>
+</template>
--- routes/dashboard.php
+++ routes/dashboard.php
@@ -4,7 +4,16 @@ use App\Models\Clearance;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/dashboard', function () {
-    // dd(Clearance::all());
-    return Inertia::render('Dashboard', ['Clearance' => Clearance::all()]);
-})->name('dashboard');
+Route::prefix("/dashboard")->group(function () {
+    Route::get('/', function () {
+        return Inertia::render('Dashboard', ['Clearance' => Clearance::all()]);
+    })->name('dashboard');
+
+    Route::get('/clearance', function () {
+        return Inertia::render('Clearance', ['Clearance' => Clearance::all()]);
+    })->name('clearance');
+
+    Route::get('/accounts', function () {
+        return Inertia::render('Accounts', ['Clearance' => Clearance::all()]);
+    })->name('accounts');
+});
